2a28a1d6b8035a0bd7cecfaadca2cbf76c178d6b
Maxym Tkachuk <s2m4f6@gmail.com>
2021-02-22 09:13:21 +0200

    Updating lab

--- lab1.py
+++ lab1.py
@@ -3,20 +3,27 @@
 class Farm:
   farm_count: int = 0
 
+  __location = ''
+  __animal_count = 0
+  __vents_power = 0
+  __area = 0
+  __monthly_income = 0 
+  __animal_species = [] 
+
   def __init__(self, location: str = "", animal_count: int = 0, vents_power: int = 0, area: int = 0, monthly_income: int = 0, animal_species: list[str] = []) -> None:
-    self.location = location
-    self.animal_count = animal_count
-    self.vents_power = vents_power
-    self.area = area
-    self.monthly_income = monthly_income
-    self.animal_species = animal_species
+    self.__location = location
+    self.__animal_count = animal_count
+    self.__vents_power = vents_power
+    self.__area = area
+    self.__monthly_income = monthly_income
+    self.__animal_species = animal_species
     Farm.farm_count += 1
 
   def __del__(self):
     Farm.farm_count -= 1
 
   def __str__(self) -> str:
-    return f"Farm:\n  location: {self.location}\n  animal count: {self.animal_count}\n  vents power: {self.vents_power}\n  area: {self.area}\n  monthly income: {self.monthly_income}\n  animal species: {self.animal_species}"
+    return f"Farm:\n  location: {self.__location}\n  animal count: {self.__animal_count}\n  vents power: {self.__vents_power}\n  area: {self.__area}\n  monthly income: {self.__monthly_income}\n  animal species: {self.__animal_species}"
 
   @classmethod
   def get_count(cls) -> int:
