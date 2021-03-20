d95be5fca9848e2a8e40d75bd96bc6acf0cb3072
Maxym Tkachuk <s2m4f6@gmail.com>
2021-03-20 20:56:10 +0200

    Added PEP8 test

---
+++ shoes/test/test_pep8.py
@@ -0,0 +1,23 @@
+import unittest
+import pep8
+
+import os
+
+def get_files(directory):
+  files = []
+  for f in os.listdir(directory):
+    name = os.path.join(directory, f)
+    if (os.path.isfile(name) and name.endswith('.py')):
+      files.append(name)
+    elif (os.path.isdir(name)):
+      files += get_files(name)
+  return files
+
+class TestPep8(unittest.TestCase):
+  def test_files(self):
+    style = pep8.StyleGuide()
+    directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    files = get_files(directory)
+    result = style.check_files(files)
+    self.assertEqual(result.total_errors, 0)
+
